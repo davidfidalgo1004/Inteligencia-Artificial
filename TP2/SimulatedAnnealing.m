58b47fca416771fe2a2c557c2330175d45233b6f
Balente <tiago.valente.scarvalho@gmail.com>
2024-11-12 12:26:11 +0000

    Update SimulatedAnnealing.m

--- TP2/SimulatedAnnealing.m
+++ TP2/SimulatedAnnealing.m
@@ -1,30 +1,38 @@
 close all
 clear all
-f = @(x) 4*(sin(5*pi*x+0.5)).^6 .* exp(log2((x-0.8).^2)); % what is this
+f = @(x) 4*(sin(5*pi*x+0.5)).^6 .* exp(log2((x-0.8).^2)); % função
 x = linspace(0,1.6,200);
 y=f(x);
 plot(x,y,'b');
 hold on
-i = 1;
 
-t = 0;
-T = Tmax;
+t = 0; % iteração
+T = 90; % temperatura max use
 nRep = 300; % n_reps
 alfa = 0.94; % fator decaimento
-dE = E_new - E(t); % delta
-p = exp(-dE/T); % probabilidade
-x(t) = rand * 1.6;
+t_rand = rand * 200;
 while(t <= nRep)
-    while n <= Tit
-        if (f(x_new)- f(x(t))) < 0 % Downhill movement
-            x(t)=x_new;
+    while (n <= )
+        xi = (rand - 0.5) * delta;
+        x_new = x_old + 2 * xi;
+        % Verifica se x_now está dentro do intervalo [0, 1.6]
+        if x_new < 0
+            x_new = 0;
+        elseif x_new > 1.6
+            x_new = 1.6;
+        end
+        dE = f(x_new)-f(xi); % delta
+        p = exp(-abs(dE)/T); % probabilidade
+        if (f(x_new)- f(x(t))) > 0 % Uphill movement
+            x(t)=x_new; 
             f(x(t))=f(x_new);
-        elseif rand(0,1)<p % Uphill movement
+        elseif rand(0,1) > p % Downhill movement
             x(t)=x_new;
             f(x(t))=f(x_new);
         end
         n=n+1;
         plot(x(t),f(x(t)), 'o')
+        fprintf("nao")
     end
     T=Tnew;
     t=t+1;
